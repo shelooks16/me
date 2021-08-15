ee33fc5e5c33788eee6c9e8173b662ab51731138
shelooks16 <blauyedz@zoho.eu>
2021-08-15 19:58:33 +0300

    setup initial appcfg

---
+++ appcfg.js
@@ -0,0 +1,39 @@
+export const appCfg = {
+  home: {
+    page: {
+      title: 'Home | shelooks16',
+      description: 'You brooooooo',
+    },
+    header: {
+      title: 'shelooks16',
+      description: 'Articles and stuff by Andrew Hulenko',
+    },
+    menu: {
+      links: [
+        {
+          to: '/reads',
+          text: '👉 Read blog',
+        },
+        {
+          to: '/me',
+          text: '😜 About me',
+        },
+      ],
+    },
+  },
+  notFound: {
+    page: {
+      title: 'Not Found ☠️ shelooks16',
+    },
+    header: {
+      title: '☠️ Not found ☠️',
+      description: 'Seems like 🤖 destroyed this page completely...',
+    },
+    backToHomeText: 'Run away to home',
+  },
+  socials: {
+    github: {
+      link: 'https://github.com/shelooks16',
+    },
+  },
+};
